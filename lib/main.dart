4c5d3f02b9630d7e7b394d98947f0b84d3acd0f9
ZZH <1462207500@qq.com>
2025-02-21 18:03:18 +0800

    save code
    Signed-off-by: ZZH <1462207500@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:timeline_tile/timeline_tile.dart';
 
 void main() {
   runApp(const TimelineApp());
@@ -18,31 +17,73 @@ class TimelineApp extends StatelessWidget {
   }
 }
 
+class Event extends StatelessWidget {
+  DateTime time;
+  String what;
+
+  Event(this.time, this.what);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(color: Colors.blue, width: 2.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '时间: ${time.toString()}',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            '事件: $what',
+            style: TextStyle(fontSize: 14.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Timeline extends StatelessWidget {
+  // final eventList = [
+  //   Event(DateTime.utc(0), '1'),
+  //   Event(DateTime.utc(3), '2'),
+  //   Event(DateTime.utc(7), '3'),
+  //   Event(DateTime.utc(27), '4'),
+  // ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onDoubleTap: () {
+          print('double click');
+        },
+        child: Container(
+          height: 120,
+          child: Event(DateTime.now(), 'assada'),
+        ),
+        // child: Container(
+        //   color: Colors.blue[300],
+        //   height: 7,
+        // ),
+      ),
+    );
+  }
+}
+
 class TimelineMainPage extends StatelessWidget {
   const TimelineMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        
-        child: Container(
-          color: Colors.blueAccent,
-        ),
-
-        // child: TimelineTile(
-        //   axis: TimelineAxis.vertical,
-        //   alignment: TimelineAlign.center,
-        //   startChild: Container(
-        //     constraints: const BoxConstraints(minHeight: 10),
-        //     color: Colors.lightGreenAccent,
-        //   ),
-        //   // endChild: Container(
-        //   //   constraints: const BoxConstraints(minHeight: 10),
-        //   //   color: Colors.lightBlueAccent,
-        //   // ),
-        // ),
-      ),
-    );
+    return Timeline();
   }
 }
